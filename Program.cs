fc9ffefca0f55e0485abab856c54e1374fed348b
wumaster1ss <88694873+wumaster1ss@users.noreply.github.com>
2022-02-18 11:26:04 -0800

    Add files via upload
    Assignment for C# - Employee Class (Project Name: EmployeeDB) - Create a class called Employee that includes three pieces of information as either instance variables or auto-implemented properties - first name (type string), last name (type string), and a monthly salary (decimal). Create 3 Employee objects and display each object’s yearly salary. Then give each Employee a 10% raise and display each Employee’s yearly salary again.
    Displaying a Bar Chart (Project Name: BarChart) - One interesting application of computers is to display graphs and bar charts. Write an app that reads three numbers between 1 and 30. For each number that’s read, your app should display the same number of adjacent asterisks. For example, if your app reads the number 7, it should display 7 stars: *******
    Guess-the-Number Game (Project Name: GuessTheNumber) - Write an app that plays “Guess the Number” as follows: Your app chooses the number to be guessed by selecting a random integer in the range 1 to 1000. The app displays the prompt "Guess a number between 1 and 1000: ". The player inputs a first guess. If the player’s guess is incorrect, your app should display Too high. Try again. or Too low. Try again.
     Computer-Assisted Instruction (Project Name: AITeacher) - The use of computers in education is referred to as computer-assisted instruction (CAI). Write a program that will help an elementary-school student learn multiplication. Use a Random object to produce two positive one-digit integers. The program should then prompt the user with a question, such as: How much is 6 times 7?

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Shawn Wu, TINFO-200A,Programming II for ITS
+// GuessTheNumber assignment - Guess-the-Number Game program.
+// This assignment has the following purpose: It will show the use of 
+// random generator , as well as demostrate the use of nested for loops
+///////////////////////////////////////////////////////////////////////////////////////////
+// Change History
+// Date         Developer        Description
+// 2022-02-05   ShawnW           Some description having to do with the creation and use of this file.
+// 2022-02-05   ShawnW           Creation of game objects.
+// 2022-02-05   ShawnW           Created nested loops.
+// 2022-02-05   ShawnW           Created different responses.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    internal class Program
+    {
+
+        // Main
+        static void Main(string[] args)
+        {
+Console.WriteLine(@"
+*****************************************************
+       Welcome to the Guess the Number program!
+*****************************************************
+This app will generate a number between 1 and 1000
+and your job is to guess the correct number. 
+");
+            //  Random number generator
+
+            Random random = new Random();
+
+            // Between 1 and 1000
+
+            int secretNumber = (random.Next()%1000) + 1;
+
+            int userGuess = 0;
+
+            // While loop to keep guess the number
+            
+            while(userGuess != secretNumber)
+            {
+                Console.Write("\nGuess a number between 1 and 1000: ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations. You guessed the number!");
+
+                    Console.Write("\nDo you want to play again? (y/n)");
+                    string ch = Console.ReadLine().ToLower();
+                    
+                    // Continue the game
+                    
+                    if (ch.Equals("y"))
+
+                    {
+                        secretNumber = (random.Next() % 1000) + 1;
+                        userGuess = 0;
+                    }
+                }
+                // Different responses based on the guess
+                 
+                else if (userGuess < secretNumber)
+                {
+                    Console.WriteLine("Too low. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Too high. Try again");
+                }
+
+                
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
